7cdb4d5ca32991f474242f6952dcdff7b3d3b103
Seba Davalle <sebastian_davalle@mcafee.com>
2020-04-24 15:07:13 -0300

    added two scripts as examples with different encoding
    Both scripts looks good in notepad++, however when try to execute the one that is not UTF8-BOM it fails.

---
+++ examples/japanese_dictionary_ok.ps1
@@ -0,0 +1,25 @@
+﻿$OutputEncoding = New-Object -typename System.Text.UTF8Encoding
+
+# Dictionaries
+$ClientToken=@{English='Client'; Spanish='cliente'; Italian='cliente'; Japanese='クライアント タイプ'};
+$IdleToken =@{English='Idle'; Spanish='inactivo'; Italian='inattivo'; Japanese='アイドル時間'};
+
+$isSpanish =  Get-UICulture | Select-String 'es-';
+$isItalian =  Get-UICulture | Select-String 'it-';
+$isJapanese =  Get-UICulture | Select-String 'ja-';
+
+if($isSpanish){
+    $ClientString = $ClientToken.Spanish;
+    $IdleString = $IdleToken.Spanish;
+}elseif($isItalian){
+    $ClientString = $ClientToken.Italian;
+    $IdleString = $IdleToken.Italian;
+}elseif($isJapanese){
+    $ClientString = $ClientToken.Japanese;
+    $IdleString = $IdleToken.Japanese;
+}else{
+    $ClientString = $ClientToken.English;
+    $IdleString = $IdleToken.English;
+}
+
+Write-Host "$ClientString - $IdleString"
